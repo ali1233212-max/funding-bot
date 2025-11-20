8611d8e5402356f16763422c16f0a32eab194629
ali1233212-max <alina.malina666@outlook.com>
2025-11-20 17:59:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ async def funding(update, context):
 
 
 # ---- Запуск бота ----
-BOT_TOKEN = "8329955590:AAH63Ax6WmTjJESyLVvqEPTE5ibutOiK_rCM"
+BOT_TOKEN = "8329955590:AAH63Ax6WmTjESyLVvqEPTE5ibutOiK_rCM"
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
